c64d204e42a599d98c89937d5b0b96abe1cb9566
Andreas Bourakis <bourakis@gmail.com>
2020-11-11 21:47:56 +0200

    Create elseif.php

---
+++ CODING/elseif.php
@@ -0,0 +1,25 @@
+<?php
+$x = 5;
+
+if ($x == 3) 
+{
+  echo "x is 3";
+} 
+elseif ($x == 4)
+{
+  echo "x is 4";
+} 
+elseif ($x == 5)
+{
+  echo "x is 5";
+} 
+  elseif ($x == 6)
+{
+  echo "x is 6";
+} 
+elseif ($x == 7)
+{
+  echo "x is 7";
+}
+
+?>
