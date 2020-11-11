4ea2c4a4ac18199d3ebd20337adc6c52fe8b312e
Andreas Bourakis <bourakis@gmail.com>
2020-11-11 21:48:47 +0200

    Update elseif.php

--- CODING/elseif.php
+++ CODING/elseif.php
@@ -1,25 +1,30 @@
+<html>
+<body>
+  
 <?php
-$x = 5;
+  $x = 5;
 
-if ($x == 3) 
-{
-  echo "x is 3";
-} 
-elseif ($x == 4)
-{
-  echo "x is 4";
-} 
-elseif ($x == 5)
-{
-  echo "x is 5";
-} 
-  elseif ($x == 6)
-{
-  echo "x is 6";
-} 
-elseif ($x == 7)
-{
-  echo "x is 7";
-}
-
+  if ($x == 3) 
+  {
+    echo "x is 3";
+  } 
+  elseif ($x == 4)
+  {
+    echo "x is 4";
+  } 
+  elseif ($x == 5)
+  {
+    echo "x is 5";
+  } 
+    elseif ($x == 6)
+  {
+    echo "x is 6";
+  } 
+  elseif ($x == 7)
+  {
+    echo "x is 7";
+  }
 ?>
+
+</body>
+</html>
