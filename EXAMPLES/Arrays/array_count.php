8b7cd07e90c2dd4abca1757433a7c08c7a0d490b
Andreas Bourakis <bourakis@gmail.com>
2020-12-16 15:04:30 +0200

    Update and rename array_functions.php to array_count.php

--- EXAMPLES/Arrays/array_count.php
+++ EXAMPLES/Arrays/array_count.php
@@ -1,7 +1,9 @@
 <?php
-$colors = array("red", "green", "blue", 4, false);
-echo count($colors);
-echo "<br>";
 
+$colors = array("red", "green", "blue", 4, false);
+
+echo count($colors);
+
+echo "<br>";
 
 ?>
