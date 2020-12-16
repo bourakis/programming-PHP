e17c6ba438582d03606f1eda06b803e22d7e4dab
Andreas Bourakis <bourakis@gmail.com>
2020-12-16 15:00:31 +0200

    Add files via upload

---
+++ EXAMPLES/Login/bool_array.php
@@ -0,0 +1,12 @@
+<?php
+$array = array(
+           "key1" => true,
+           "key2" => false,
+           "key3" => false,
+           "key4" => true
+         );
+foreach ($array as $value){
+	echo $value . "<br>"; 
+}
+
+?>
