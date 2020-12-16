2369d7997fc9fa15efe2cc5156f13272ff99b2c4
Andreas Bourakis <bourakis@gmail.com>
2020-12-16 15:12:19 +0200

    Update sort_array.php

--- EXAMPLES/Arrays/sort_array.php
+++ EXAMPLES/Arrays/sort_array.php
@@ -1,11 +1,13 @@
 <?php
 $cars=array("Volvo","BMW","Toyota");
+
 sort($cars);
 
 $clength=count($cars);
-for($x=0;$x<$clength;$x++)
-  {
-  echo $cars[$x];
-  echo "<br>";
-  }
+
+for($x=0; $x < $clength; $x++)
+{
+    echo $cars[$x];
+    echo "<br>";
+}
 ?>
