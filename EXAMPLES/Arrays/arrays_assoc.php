92287428193aa23d6c4a2fd9026d7038035aa381
Andreas Bourakis <bourakis@gmail.com>
2021-01-09 13:20:06 +0200

    Update and rename arraya.php to arrays_assoc.php

--- EXAMPLES/Arrays/arrays_assoc.php
+++ EXAMPLES/Arrays/arrays_assoc.php
@@ -1,9 +1,11 @@
 <?php
+
 $prices['Tires'] = 100;
 $prices['Oil'] = 10;
 $prices['Spark Plugs'] = 4;
 
 
 foreach ($prices as $key => $value)
- echo $key.'=>'.$value.'<br />';
+    echo $key . '=>' . $value . '<br />';
+
 ?>
