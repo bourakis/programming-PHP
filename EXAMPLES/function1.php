08143f05aae37dd0a2d0d17743233ad6cc577df2
Andreas Bourakis <bourakis@gmail.com>
2021-01-09 13:20:54 +0200

    Add files via upload

---
+++ EXAMPLES/function1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title></title>
+   </head>
+   <body>
+      <?php
+        
+        function writeMessage() {
+            echo "You are really a nice person, Have a nice time!";
+         }
+         
+         
+         writeMessage();
+      ?>
+   </body>
+</html>
+<html>
+   
+  
+      
+      
+      
+ 
