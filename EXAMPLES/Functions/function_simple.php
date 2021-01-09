3535787600e6264c02edadf97d5a5b4afb317d4f
Andreas Bourakis <bourakis@gmail.com>
2021-01-09 13:22:19 +0200

    Update and rename EXAMPLES/function1.php to EXAMPLES/Functions/function_simple.php

---
+++ EXAMPLES/Functions/function_simple.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+<?php
+
+function writeMessage() 
+{
+    echo "You are really a nice person, Have a nice time!";
+}
+
+writeMessage();
+?>
+    
+</body>
+</html>
+<html>
+
+
+      
+      
+      
+ 
