5943c65d5fa07243f3a9cb5a4f676b32b412014e
Andreas Bourakis <bourakis@gmail.com>
2021-01-09 13:15:22 +0200

    Update and rename associative_array.php to arrays_associative.php

---
+++ EXAMPLES/Arrays/arrays_associative.php
@@ -0,0 +1,17 @@
+<?php
+
+// Linear Array
+$data = array("Bill", "Gates", 43);
+
+echo $data[0];
+
+echo "<br>";
+
+// Associative Array
+$data1 = array(
+	"fname" => "Bill", 
+	"lname" => "Gates",
+	"age"   => 40);
+
+echo $data1["fname"];
+?>
